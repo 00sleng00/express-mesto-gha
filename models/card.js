eb2b395d2b42fcda92d973eab844272390de3605
00sleng00 <roman.morshikoff@yandex.ru>
2022-06-06 00:23:43 +0300

    Проектная работа№14

--- models/card.js
+++ models/card.js
@@ -1,31 +1,36 @@
 const mongoose = require('mongoose');
 
+const { reg } = require('../utils/constants');
+
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
     minlength: 2,
     maxlength: 30,
-    required: true,
   },
   link: {
     type: String,
     required: true,
+    validate: {
+      validator(v) {
+        return reg.test(v);
+      },
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
     required: true,
+    ref: 'user',
   },
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      default: [],
-    },
-  ],
+  likes: {
+    type: Array,
+    default: [],
+  },
   createdAt: {
     type: Date,
     default: Date.now,
   },
-});
+}, { versionKey: false });
 
 module.exports = mongoose.model('card', cardSchema);
