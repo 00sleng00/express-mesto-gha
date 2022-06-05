22db5a7272f6d113d804881a87b9cb11823f106f
00sleng00 <roman.morshikoff@yandex.ru>
2022-06-05 15:29:18 +0300

    Проектная работа №14

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
-const { generateToken, isAuthorization } = require('../utils/jwt');
+const { generateToken } = require('../utils/jwt');
 
 const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
@@ -10,33 +10,26 @@ const ServerError = require('../errors/ServerErrror');
 
 const createUser = async (req, res, next) => {
   const {
-    email, password, name, about, avatar,
+    name, about, avatar, email, password,
   } = req.body;
+
   if (!email || !password) {
-    next(new AuthorizationError('Неверный логин или пароль'));
-    return;
+    return next(new ValidationError('Неверный логин или пароль'));
   }
-  try {
-    const hash = await bcrypt.hash(password, 10);
-    const userCreate = new User({
-      email,
-      password: hash,
-      name,
-      about,
-      avatar,
+  return bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name, about, avatar, email, password: hash,
+    }))
+    .then((user) => res.status(201).send({
+      name: user.name, about: user.about, avatar: user.avatar, email: user.email, id: user._id,
+    }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new ConflictError('Такой пользователь уже существует'));
+      } else {
+        next(err);
+      }
     });
-    res.status(201).send(await userCreate.save());
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      next(new ValidationError('Ошибка валидации'));
-      return;
-    }
-    if ((err.code === 11000)) {
-      next(new ConflictError('Такой пользователь уже существует'));
-      return;
-    }
-    next(new ServerError('Произошла ошибка сервера'));
-  }
 };
 
 const login = async (req, res, next) => {
@@ -64,77 +57,72 @@ const login = async (req, res, next) => {
   }
 };
 
-const getUsers = async (_req, res, next) => {
-  try {
-    const users = await User.find({});
-    res.status(200).send(users);
-  } catch (err) {
-    next(new ServerError('Произошла ошибка сервера'));
-  }
+const getUsers = async (_, res, next) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch(next);
 };
 
 const getUser = async (req, res, next) => {
-  const isAuth = await isAuthorization(req.headers.authorization);
-  if (!isAuth) {
-    next(new AuthorizationError('Нет доступа'));
-    return;
-  }
-  try {
-    const user = await User.findOne({ id: req.params.id });
-    if (!user) {
-      next(new NotFoundError('Пользователя нет в базе данных'));
-      return;
-    }
-    res.status(200).send({ data: user });
-  } catch (err) {
-    if (err.name === 'CastError') {
-      next(new ValidationError('Пользователь не найден'));
-      return;
-    }
-    next(new ServerError('Произошла ошибка сервера'));
-  }
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        return next(new NotFoundError('Пользователь не найден'));
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new ValidationError('Пользователя с таким id нет'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const getUserId = async (req, res, next) => {
-  const isAuth = await isAuthorization(req.headers.authorization);
-  if (!isAuth) {
-    next(new AuthorizationError('Нет доступа'));
-    return;
-  }
-  try {
-    const userId = await User.findById(req.params.userId);
-    if (!userId) {
-      next(new NotFoundError('Пользователя нет в базе данных'));
-      return;
-    }
-    res.status(200).send({ data: userId });
-  } catch (err) {
-    if (err.name === 'CastError') {
-      next(new ValidationError('Пользователя с таким id нет'));
-      return;
-    }
-    next(new ServerError('Произошла ошибка сервера'));
-  }
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        return next(new NotFoundError('Пользователь не найден'));
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new ValidationError('Пользователя с таким id нет'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const updateUser = async (req, res, next) => {
-  try {
-    const { name, about } = req.body;
-    const userUpdate = await User.findByIdAndUpdate(
-      req.user._id,
-      { name, about },
-      { new: true, runValidators: true },
-    );
-    res.status(200).send(userUpdate);
-  } catch (err) {
-    if (err.name === 'ValidationError') {
-      next(new ValidationError(`${Object.values(err.errors)
-        .map((error) => error.message)
-        .join(', ')}`));
-      return;
-    }
-    next(new ServerError('Произошла ошибка сервера'));
-  }
+  const { name, about } = req.body;
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => {
+      if (!user) {
+        return next(new NotFoundError('Пользователь не найден'));
+      }
+      return res.status(200).send({
+        name: user.name,
+        about: user.about,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new ValidationError('Переданы некорректные данные'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const updateAvatar = async (req, res, next) => {
@@ -149,7 +137,7 @@ const updateAvatar = async (req, res, next) => {
   )
     .then((user) => {
       if (!user) {
-        return next(new NotFoundError('Переданы некорректные данные'));
+        return next(new NotFoundError('Пользователь не найден'));
       }
       return res.status(200).send({
         avatar: user.avatar,
--- utils/jwt.js
+++ utils/jwt.js
@@ -4,16 +4,6 @@ const JWT_SECRET_KEY = '1234567890987654321';
 // eslint-disable-next-line no-return-await
 const generateToken = async (id) => await jwt.sign({ id }, JWT_SECRET_KEY, { expiresIn: '7d' });
 
-const isAuthorization = async (token) => {
-  try {
-    const decoded = await jwt.verify(token, JWT_SECRET_KEY);
-    return !!decoded;
-  } catch (err) {
-    return false;
-  }
-};
-
 module.exports = {
   generateToken,
-  isAuthorization,
 };
