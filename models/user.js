f9f4fd4bbba04ec5e5ea52f8ff9362c3a4c79b0c
00sleng00 <roman.morshikoff@yandex.ru>
2022-06-06 00:17:24 +0300

    Проектная работа №14

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 const { reg } = require('../utils/constants');
 
@@ -38,5 +39,21 @@ const userSchema = new mongoose.Schema({
     select: false,
   },
 }, { versionKey: false });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error({ message: 'Неверный логин или пароль' }));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error({ message: 'Неверный логин или пароль' }));
+          }
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
